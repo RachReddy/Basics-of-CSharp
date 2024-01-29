ba7ea61067d2bb628ae06e04eebd0bcec29b3c08
Rachana NV <94166047+RachReddy@users.noreply.github.com>
2024-01-29 18:34:49 +0530

    Add files via upload

---
+++ Strings/StringBuffer_Example.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBasics
+{
+    public class StringBuffer_Example
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Hello");
+            sb.Append(" World");
+            sb.Append("!");
+
+            sb.Insert(5, "beautiful ");
+
+            int length = sb.Length;
+            Console.WriteLine(length); // Output: 19
+
+            //accessing
+            char firstChar = sb[0];
+            Console.WriteLine(firstChar); // Output: H
+
+            //sb.Clear();
+
+            //Convert StringBuilder to string using ToString()
+            string result = sb.ToString(); 
+            Console.WriteLine(result); // Output: Hello beautiful World!
+
+            sb.Remove(6, 10); // Removes "beautiful "
+
+            int capacity = sb.Capacity;
+            Console.WriteLine(capacity); // Output: Capacity of the StringBuilder
+
+        }
+    }
+}
---
+++ Strings/Strings_Are_Immutable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBasics
+{
+    public class Strings_Are_Immutable
+    {
+        static void Main()
+        {
+            string originalString = "Hello";
+            Console.WriteLine("Original string: " + originalString);
+
+            string modifiedString = originalString + " World";
+
+            // Print the modified string
+            Console.WriteLine("Modified string: " + modifiedString);
+
+            // Print the original string to show it remains unchanged
+            Console.WriteLine("Original string (unchanged): " + originalString);
+        }
+    }
+}
+
+//Original string: Hello
+//Modified string: Hello World
+//Original string (unchanged): Hello
